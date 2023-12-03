148d17b996ff1ed6c311727e46028b7c1147b505
vicjuma <vicjuma945@gmail.com>
2023-12-03 15:58:23 +0300

    add connection to mongo atlas cloud

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/101394258_comp3123_assignment2_db");
+// mongoose.connect("mongodb://localhost:27017/101394258_comp3123_assignment2_db");
+const uri = "mongodb+srv://oluochvee:jumavic9118@cluster0.rspbp3j.mongodb.net/?retryWrites=true&w=majority/employees";
+mongoose.connect(uri);
 
 const db = mongoose.connection;
 
